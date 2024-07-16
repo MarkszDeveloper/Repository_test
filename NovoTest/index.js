641c27acecddff706426925f36e3994bf9fa49b2
Marco Antonio Corrêa Araújo <marcoaraujo.developer@gmail.com>
2024-07-16 14:47:01 -0300

    Delete NovoTest/index.js

--- NovoTest/index.js
+++
@@ -1,6 +0,0 @@
-function teste() {
-    let argumento1 = document.getElementById('text1');
-    let local = document.getElementById('res');
-    local.innerHTML = `Olá ${argumento1}!`; 
-    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.innerHTML;
-}
