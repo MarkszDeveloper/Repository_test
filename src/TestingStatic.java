b36c381e34ef99feabb6fb044eaf129f3bcabb9d
Marksz <marcoaraujo.developer@gmail.com>
2024-07-20 10:48:18 -0300

    adding: new lines TestingStatic.java /v3

--- src/TestingStatic.java
+++ src/TestingStatic.java
@@ -2,7 +2,7 @@ public class TestingStatic {
     public static void main(String[] args) {
         System.out.println("Nova alteração :)");
         System.out.println("Olá");
-
+        System.out.println("Olá")
 
     }
 }
